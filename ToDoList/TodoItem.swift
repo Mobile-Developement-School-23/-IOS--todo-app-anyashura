46f1ced8a91e4ddaa5ee1e3946574fb8fb830f50
anyashura <99889852+anyashura@users.noreply.github.com>
2023-06-17 01:50:18 +0300

    Hometask1
    Created filsed TodoItem and Filecache and tests for todoitem

---
+++ ToDoList/TodoItem.swift
@@ -0,0 +1,177 @@
+//
+//  TodoItem.swift
+//  ToDoList
+//
+//  Created by Anna Shuryaeva on 11.06.2023.
+//
+
+import Foundation
+
+struct TodoItem {
+    
+    // MARK: - Enum
+    enum Importance: String {
+        case low
+        case normal
+        case high
+    }
+    // MARK: - Properties
+    let id: String
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isDone: Bool
+    let dateCreated: Date
+    let dateEdited: Date?
+    
+    // MARK: - Init
+    init(
+        id: String = UUID().uuidString,
+        text: String,
+        importance: Importance,
+        deadline: Date? = nil,
+        isDone: Bool,
+        dateCreated: Date = Date.now,
+        dateEdited: Date? = nil
+    ) {
+        self.id = id
+        self.text = text
+        self.importance = importance
+        self.deadline = deadline
+        self.isDone = isDone
+        self.dateCreated = dateCreated
+        self.dateEdited = dateEdited
+    }
+}
+
+// MARK: - Extensions
+extension TodoItem {
+    var json: Any {
+        var dictionary: [String: Any] = [:]
+        
+        dictionary[Constants.id] = self.id
+        dictionary[Constants.text] = self.text
+        dictionary[Constants.isDone] = self.isDone
+        dictionary[Constants.dateCreated] = self.dateCreated.timeStamp
+        if importance != .normal {
+            dictionary[Constants.importance] = self.importance.rawValue
+        }
+        if let deadline = deadline {
+            dictionary[Constants.deadline] = deadline.timeStamp
+        }
+        if let dateEdited = dateEdited {
+            dictionary[Constants.dateEdited] = dateEdited.timeStamp
+        }
+        return dictionary
+    }
+    
+    var csvString: String {
+        var importanceForCSV = ""
+        if importance != .normal {
+            importanceForCSV = self.importance.rawValue
+        }
+        var deadlineForCSV = ""
+        if let deadline = deadline {
+            deadlineForCSV = String(deadline.timeStamp)
+        }
+        var dateEditedForCSV = ""
+        if let dateEdited = dateEdited {
+            dateEditedForCSV = String(dateEdited.timeStamp)
+        }
+        let text = TodoItem.replaceSymbols(in: self.text, what: ",", with: "~")
+    
+        return "\(self.id),\(text),\(importanceForCSV),\(deadlineForCSV), \(String(self.isDone)),\(String(self.dateCreated.timeStamp)),\(dateEditedForCSV)"
+    }
+    
+    static func parseCSV(csvString: String) -> TodoItem? {
+        let values = csvString.components(separatedBy: ",")
+        let id = values[0]
+        let text = replaceSymbols(in: values[1], what: "~", with: ",")
+        var isDone = false
+        if values[4] == " true" {
+            isDone = true
+        }
+        var deadline: Date? = nil
+        if values[3] != "" {
+            deadline = Int(values[3])?.dateFormat
+        }
+        let dateCreated = Int(values[5])?.dateFormat ?? Date.now
+        var dateEdited: Date? = nil
+        if values[6] != "" {
+            dateEdited = Int(values[3])?.dateFormat
+        }
+        let importance = Importance(rawValue: values[2]) ?? Importance.normal
+        
+        return TodoItem(
+            id: id,
+            text: text,
+            importance: importance,
+            deadline: deadline,
+            isDone: isDone,
+            dateCreated: dateCreated,
+            dateEdited: dateEdited
+        )
+    }
+    
+    static func parse(json: Any) -> TodoItem? {
+        guard let dictionary = json as? [String: Any] else {
+            return nil
+        }
+        guard let id = dictionary[Constants.id] as? String,
+              let text = dictionary[Constants.text] as? String,
+              let isDone = dictionary[Constants.isDone] as? Bool,
+              let dateCreated = (dictionary[Constants.dateCreated] as? Int)?.dateFormat else {
+            return nil
+        }
+        var importance = Importance.normal
+        if let importanceString = dictionary[Constants.importance] as? String,
+           let importanceFromDict = Importance(rawValue: importanceString) {
+            importance = importanceFromDict
+        }
+        let deadline = (dictionary[Constants.deadline] as? Int)?.dateFormat
+        let dateEdited = (dictionary[Constants.dateEdited] as? Int)?.dateFormat
+        
+        return TodoItem(
+            id: id,
+            text: text,
+            importance: importance,
+            deadline: deadline,
+            isDone: isDone,
+            dateCreated: dateCreated,
+            dateEdited: dateEdited
+        )
+    }
+    
+    //function for corner case, when in field "text" there is a symbol the same as separator
+     static func replaceSymbols(in string: String, what replace: Character, with replacement: Character) -> String {
+        return String(string.map { $0 == replace ? replacement : $0 })
+    }
+}
+
+// convert Date to Int
+extension Date {
+    var timeStamp: Int {
+        Int(self.timeIntervalSince1970)
+    }
+}
+
+// convert Int to Date
+extension Int {
+    var dateFormat: Date? {
+        return Date(timeIntervalSince1970: Double(self))
+    }
+}
+
+// MARK: - Constants
+extension TodoItem {
+    
+    enum Constants {
+        static let id = "id"
+        static let text = "text"
+        static let importance = "importance"
+        static let deadline = "deadline"
+        static let isDone = "isDone"
+        static let dateCreated = "dateCreated"
+        static let dateEdited = "dateEdited"
+    }
+}
---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,230 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Anna Shuryaeva on 11.06.2023.
+//
+
+import XCTest
+@testable import ToDoList
+
+final class ToDoListTests: XCTestCase {
+    
+    //MARK: - Tests for TodoItem
+    //Сделала тесты только для TodoItem (это и просили в ТЗ)
+    func testParsingJSONWithoutOptionals() throws {
+        let json: [String:Any] =
+        [
+            "id": "id",
+            "text": "text",
+            "isDone": true,
+            "importance": "low",
+            "dateCreated": 1686945208
+        ]
+        guard let item = TodoItem.parse(json: json) else{
+            XCTFail("Не получилось распарсить JSON в todoItem")
+            return
+        }
+        XCTAssertEqual(item.id, "id", "Парсинг id")
+        XCTAssertEqual(item.text, "text", "Парсинг текста")
+        XCTAssertEqual(item.importance, TodoItem.Importance.low, "Парсинг важности")
+        XCTAssertEqual(item.deadline, nil, "Парсинг даты дедлайна")
+        XCTAssertEqual(item.dateCreated, 1686945208.dateFormat, "Парсинг даты создания")
+        XCTAssertEqual(item.dateEdited, nil, "Парсинг даты редактирования")
+        XCTAssertEqual(item.isDone, true, "Парсинг статуса готовности")
+    }
+    
+    func testParsingJSONWithNormalImportanceAndAllFields() throws {
+        let json: [String:Any] =
+        [
+            "id": "id",
+            "text": "text",
+            "isDone": false,
+            "dateCreated": 1686945208,
+            "dateEdited": 1686945209,
+            "deadline": 1686945309
+        ]
+        guard let item = TodoItem.parse(json: json) else{
+            XCTFail("Не получилось распарсить JSON в todoItem")
+            return
+        }
+        XCTAssertEqual(item.id, "id", "Парсинг id")
+        XCTAssertEqual(item.text, "text", "Парсинг текста")
+        XCTAssertEqual(item.importance, TodoItem.Importance.normal, "Парсинг важности")
+        XCTAssertEqual(item.deadline, 1686945309.dateFormat, "Парсинг даты дедлайна")
+        XCTAssertEqual(item.dateCreated, 1686945208.dateFormat, "Парсинг даты создания")
+        XCTAssertEqual(item.dateEdited, 1686945209.dateFormat, "Парсинг даты редактирования")
+        XCTAssertEqual(item.isDone, false, "Парсинг статуса готовности")
+    }
+    
+    func testParsingCSVWithoutOptionals() throws {
+        let csvString = "5A0C0E34-B9F6-4C0B-963B-8E4838AAD99E,text,high,, true,1686946798,"
+        guard let item = TodoItem.parseCSV(csvString: csvString) else{
+            XCTFail("Не получилось распарсить CSV в todoItem")
+            return
+        }
+        XCTAssertEqual(item.id, "5A0C0E34-B9F6-4C0B-963B-8E4838AAD99E", "Парсинг id")
+        XCTAssertEqual(item.text, "text", "Парсинг текста")
+        XCTAssertEqual(item.importance, TodoItem.Importance.high, "Парсинг важности")
+        XCTAssertEqual(item.deadline, nil, "Парсинг даты дедлайна")
+        XCTAssertEqual(item.dateCreated, 1686946798.dateFormat, "Парсинг даты создания")
+        XCTAssertEqual(item.dateEdited, nil, "Парсинг даты редактирования")
+        XCTAssertEqual(item.isDone, true, "Парсинг статуса готовности")
+    }
+    
+    func testParsingCSVWithNormalImportanceAndAllFields() throws {
+        let csvString = "id,text,,1686947257, false,1686947257,1686947257"
+        guard let item = TodoItem.parseCSV(csvString: csvString) else{
+            XCTFail("Не получилось распарсить CSV в todoItem")
+            return
+        }
+        XCTAssertEqual(item.id, "id", "Парсинг id")
+        XCTAssertEqual(item.text, "text", "Парсинг текста")
+        XCTAssertEqual(item.importance, TodoItem.Importance.normal, "Парсинг важности")
+        XCTAssertEqual(item.deadline, 1686947257.dateFormat, "Парсинг даты дедлайна")
+        XCTAssertEqual(item.dateCreated, 1686947257.dateFormat, "Парсинг даты создания")
+        XCTAssertEqual(item.dateEdited, 1686947257.dateFormat, "Парсинг даты редактирования")
+        XCTAssertEqual(item.isDone, false, "Парсинг статуса готовности")
+    }
+    
+    func testConvertStructToCsvWithNormalImportanceAndAllFields() {
+        let deadline = Date(timeIntervalSinceNow: 86400)
+        let dateCreated = Date()
+        let dateEdited = Date(timeIntervalSinceNow: 3600)
+        
+        let item = TodoItem(
+            id: "123",
+            text: "text",
+            importance: TodoItem.Importance.normal,
+            deadline: Date(timeIntervalSinceNow: 86400),
+            isDone: false,
+            dateCreated: Date(),
+            dateEdited: Date(timeIntervalSinceNow: 3600)
+        )
+        
+        let csvString = item.csvString
+        
+        XCTAssertEqual(csvString, "123,text,,\(Int(deadline.timeIntervalSince1970)), false,\(Int(dateCreated.timeIntervalSince1970)),\(Int(dateEdited.timeIntervalSince1970))", "Получение строки из todoItem")
+    }
+    
+    func testConvertStructToCsvWithoutOptionals() {
+        let dateCreated = Date()
+        
+        let item = TodoItem(
+            id: "123",
+            text: "text",
+            importance: TodoItem.Importance.high,
+            isDone: true,
+            dateCreated: Date()
+        )
+        let csvString = item.csvString
+        
+        XCTAssertEqual(csvString, "123,text,high,, true,\(Int(dateCreated.timeIntervalSince1970)),", "Получение строки из todoItem")
+    }
+    
+    func testConvertStructToJsonWithNormalImportanceAndAllFields() {
+        let id = "123"
+        let text = "Some text"
+        let importance = TodoItem.Importance.normal
+        let deadline = Date(timeIntervalSinceNow: 86400)
+        let isDone = false
+        let dateCreated = Date()
+        let dateEdited = Date(timeIntervalSinceNow: 3600)
+        
+        let item = TodoItem(
+            id: id,
+            text: text,
+            importance: importance,
+            deadline: deadline,
+            isDone: isDone,
+            dateCreated: dateCreated,
+            dateEdited: dateEdited
+        )
+        
+        let json = item.json as? [String: Any]
+        
+        XCTAssertEqual(json?[TodoItem.Constants.id] as? String, id)
+        XCTAssertEqual(json?[TodoItem.Constants.text] as? String, text)
+        XCTAssertEqual(json?[TodoItem.Constants.deadline] as? Int, Int(deadline.timeIntervalSince1970))
+        XCTAssertNil(json?[TodoItem.Constants.importance])
+        XCTAssertEqual(json?[TodoItem.Constants.isDone] as? Bool, isDone)
+        XCTAssertEqual(json?[TodoItem.Constants.dateCreated] as? Int, Int(dateCreated.timeIntervalSince1970))
+        XCTAssertEqual(json?[TodoItem.Constants.dateEdited] as? Int, Int(dateEdited.timeIntervalSince1970))
+    }
+    
+    func testConvertStructToJsonWithoutOptionals() {
+        let id = "123"
+        let text = "Some text"
+        let importance = TodoItem.Importance.low
+        let deadline: Date? = nil
+        let isDone = false
+        let dateCreated = Date()
+        let dateEdited: Date? = nil
+        
+        let item = TodoItem(
+            id: id,
+            text: text,
+            importance: importance,
+            deadline: deadline,
+            isDone: isDone,
+            dateCreated: dateCreated,
+            dateEdited: dateEdited
+        )
+        
+        let json = item.json as? [String: Any]
+        XCTAssertEqual(json?[TodoItem.Constants.id] as? String, id)
+        XCTAssertEqual(json?[TodoItem.Constants.text] as? String, text)
+        XCTAssertNil(json?[TodoItem.Constants.deadline])
+        XCTAssertNil(json?[TodoItem.Constants.dateEdited])
+        XCTAssertEqual(json?[TodoItem.Constants.isDone] as? Bool, isDone)
+        XCTAssertEqual(json?[TodoItem.Constants.dateCreated] as? Int, Int(dateCreated.timeIntervalSince1970))
+    }
+    func testConvertStructToCsvWithSeparatorInName() {
+        let item = TodoItem(
+            id: "123",
+            text: "text, text, text",
+            importance: TodoItem.Importance.normal,
+            isDone: false,
+            dateCreated: Date.now
+        )
+        
+        let csvString = item.csvString
+        
+        XCTAssertEqual(csvString, "123,text~ text~ text,,, false,\(Date.now.timeStamp),", "Получение строки из todoItem со знаком сепаратора внутри")
+    }
+    
+    func testJSONInAnotherFormat() {
+        let json: [Int:Any] =
+        [
+            1: "id",
+            2: "text"
+        ]
+        let item = TodoItem.parse(json: json)
+        XCTAssertNil(item)
+    }
+    
+    func testParsingJSONWithNil() throws {
+        let json: [String:Any] =
+        [
+            "id": 1,
+            "text": 1,
+            "isDone": 1,
+            "dateCreated": "1686945208"
+        ]
+        let item = TodoItem.parse(json: json)
+        XCTAssertNil(item, "Неправильный формат обязательных полей")
+            
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
